fef049280d41b8975f53c5de8547b27d6af144ce
Shaun Howard <smh150@case.edu>
2017-02-02 10:47:45 -0500

    everything ready to test on server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eecs438_hw1
 Matrix multiplication implementation and benchmark in C using XSEDE.
+
+author: Shaun Howard (smh150@case.edu)
--- hw1_matrix_mult.c
+++ hw1_matrix_mult.c
@@ -1,44 +1,86 @@
 /*
 
-EECS 438 HW1
+EECS 438, HW1
 
-Standard matrix multiplication using row-major and column-major order.
+Run either a standard row-major order or row-major order for m_one and column-major order for m_two matrix multiplication.
+It starts with a square matrix of N=1000 and increases each dimension by 100 over 10 loops.
+Matrices contain generated double-precision floating point numbers.
+The first and only argument is either 1 for row major or 0 for mixed row/column major.
 
-Shaun Howard (smh150@case.edu)
+author: Shaun Howard (smh150@case.edu)
 
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
+// define bool values
+typedef int bool;
+#define true 1
+#define false 0
 
-/*
-ADD TIMING CODE!!
-*/
+bool PRINT_OUTPUTS = false;
 
-mat_mul(double* m_one, double* m_two, double* m_product, bool row_major) {
+
+void mat_mul(double* m_one, double* m_two, double* m_product, bool row_major, int n_rows_one,
+		     int n_cols_one, int n_rows_two, int n_cols_two) {
 	int i, j, k;
+
+	if (n_rows_two != n_cols_one){
+		// sanity check to see if matrices can be multiplied
+		printf("Inner matrix dimensions are not the same, cannot continue.");
+		return;
+	}
+
+	if (PRINT_OUTPUTS) {
+		// print out first input matrix
+		printf("First input matrtix:\n");
+		for(i = 0; i < n_rows_one; i++){
+		    for(j = 0; j < n_cols_one; j++){
+			    printf("%f ", m_one[i*n_cols_one+j]);
+		    }
+		    printf("\n");
+		}
+
+		printf("\n");
+
+		// print out first input matrix
+		printf("Second input matrix:\n");
+		for(i = 0; i < n_rows_two; i++){
+		    for(j = 0; j < n_cols_two; j++){
+			    printf("%f ", m_two[i*n_cols_two+j]);
+		    }
+		    printf("\n");
+		}
+		printf("\n");
+	}
 
 	// begin timer
 	clock_t begin = clock();
 
-	if (row_major == true) {}
-		// Multiply m_one and m_two using row major indexing order
+	if (row_major == true) {
+		// Multiply m_one and m_two using row-major indexing order
 		// offset = (row x NUM_COLS) + col
 		for(i = 0; i < n_rows_one; i++){
 		    for(j = 0; j < n_cols_two; j++){
+		    	// initialize product value at i,j to 0.0
+		    	m_product[i*n_cols_two+j] = 0.0;
    				for(k = 0; k < n_rows_two; k++){
-					m_product[i*n_cols_two+k] += m_one[i*n_cols_one+k] * m_two[k*n_cols_two+j];
+   					// here, we know n_cols_one == n_rows_two
+					m_product[i*n_cols_two+j] += m_one[i*n_cols_one+k] * m_two[k*n_cols_two+j];
 				}
 			} 
 		}
 	} else {
-		// Multiply m_one and m_two using column major indexing order for array B
+		// Multiply m_one with row-major order and m_two with column-major order
 		// offset = (col x NUM_ROWS) + row
 		for(i = 0; i < n_rows_one; i++){
-			for(k = 0; k < n_rows_two; k++){
-			    for(j = 0; j < n_cols_two; j++){
-					m_product[i*n_cols_two+k] += m_one[i*n_cols_one+k] * m_two[j*n_rows_two+k];
+		    for(j = 0; j < n_cols_two; j++){
+		    	// initialize product value at i,j to 0.0
+		    	m_product[i*n_cols_two+j] = 0.0;
+   				for(k = 0; k < n_rows_two; k++){
+					m_product[i*n_cols_two+j] += m_one[i*n_cols_one+k] * m_two[j*n_rows_two+k];
 				} 
 		    }
 		}
@@ -48,48 +90,77 @@ mat_mul(double* m_one, double* m_two, double* m_product, bool row_major) {
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-	printf("Matrix multiplication of matrices took %.3f seconds.\n", time_spent);
+	if (time_spent < 0.01)
+		printf("Matrix multiplication of matrices took %.5f milliseconds.\n", time_spent*1000);
+	else
+		printf("Matrix multiplication of matrices took %.5f seconds.\n", time_spent);
 
-	// print out the product matrix
-	printf("Output Product Matrix\n");
-	for(i = 0; i < n_rows_one; i++){
-	    for(j = 0; j < n_cols_two; j++){
-		    printf("%d ", m_product[i][j]);
-	    }
-	    printf("\n");
+
+	if (PRINT_OUTPUTS){
+		// print out the product matrix
+		printf("Output Product Matrix\n");
+		for(i = 0; i < n_rows_one; i++){
+		    for(j = 0; j < n_cols_two; j++){
+			    printf("%f ", m_product[i*n_cols_two+j]);
+		    }
+		    printf("\n");
+		}
+		printf("\n");
 	}
+	return;
 }
  
-int main(){
+int main(int argc, char *argv[]){
+	/*
+	Run either a standard row-major order or row-major order for m_one and column-major order for m_two matrix multiplication.
+	It starts with a square matrix of N=1000 and increases each dimension by 100 over 10 loops.
+	Matrices contain generated double-precision floating point numbers.
+	The first and only argument is either 1 for row major or 0 for mixed row/column major.
+	*/
 	int main_loop_counter = 0;
-	int starting_matrix_size = 50;
-	int n_loops = 10:
+	int starting_matrix_size = 1000;
+	int mat_size_incr = 100;
+	int n_loops = 10;
+	int i, j;
+    int curr_mat_size = starting_matrix_size;
+    bool row_major = atoi(argv[1]);
+
+    if (row_major)
+	    printf("Starting matrix multiplication with row-major indexing.\n");
+	else
+	    printf("Starting matrix multiplication with row-major indexing for m_one and column-major indexing for m_two.\n");
+
 
 	for (main_loop_counter=0; main_loop_counter < n_loops; main_loop_counter++) {
-	    // multiply all matrices in row major and time MM operations
-	    // increase the matrix size during each loop, building up to starting_size * n_loops
-	    int curr_mat_size = starting_matrix_size * (main_loop_counter+1) 
+	    // multiply all matrices in chosen order and time MM operations
+	    // increase the matrix size by mat_size_incr after each loop
+
+	    // create curr_mat_size x curr_mat_size matrix arrays
 		double* m_one = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
-		double* m_one = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
+		double* m_two = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
+		double* m_product = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
 
-		double m_product[n_rows_one][n_cols_two] = {0.0};
-		int n_rows_one, n_cols_one;
-		int n_rows_two, n_cols_two;
+		// initialize arrays with somewhat random, but predictable values
+		for(i=0; i<curr_mat_size;i++)
+			for(j=0; j<curr_mat_size;j++)
+                m_one[i*curr_mat_size+j]=i+j;
 
-		mat_mul(m_one, m_two, m_product, true);
+        // use same initialization as for m_one, but add one to each value
+		for(i=0; i<curr_mat_size;i++)
+			for(j=0; j<curr_mat_size;j++)
+                m_two[i*curr_mat_size+j]=i+j+1;
 
-	for (main_loop_counter=0; main_loop_counter < n_loops; main_loop_counter++) {
-	    // multiply all matrices in column major and time MM operations
-	    // increase the matrix size during each loop, building up to starting_size * n_loops
-		    int curr_mat_size = starting_matrix_size * (main_loop_counter+1) 
-		double* m_one = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
-		double* m_one = malloc(sizeof(double) * curr_mat_size * curr_mat_size);
+		// store matrix sizes
+		int n_rows_one = curr_mat_size, n_cols_one = curr_mat_size;
+		int n_rows_two = curr_mat_size, n_cols_two = curr_mat_size;
 
-		double m_product[n_rows_one][n_cols_two] = {0.0};
-		int n_rows_one, n_cols_one;
-		int n_rows_two, n_cols_two;
-
-		mat_mul(m_one, m_two, m_product, false);
+		// compute matrix multiplication
+		mat_mul(m_one, m_two, m_product, row_major, n_rows_one, n_cols_one, n_rows_two, n_cols_two);
+		free(m_one);
+		free(m_two);
+		free(m_product);
+		curr_mat_size += mat_size_incr;
+	}
 
     return 0;
 }
